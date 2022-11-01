41d10fb5520366579d49a5e71cfc091629f0b865
Kevinhansson17 <kihe1997@live.com.ar>
2022-11-01 18:22:12 -0300

    Finished Homework JSII

---
+++ 02-JS-I/homework/Respuestas teoria JV I.rb
@@ -0,0 +1,5 @@
+// Variables: son espacios de memoria donde se almacenan datos que pueden ser usados mas tarde, y se les da un valor arbitrario
+// Strings: es un tipo de dato que se basa en cadenas de texto, y son definidas con comiilas simples '' o dobles ""
+// Funciones: son objetos invocables que siempre devuelven un valor y nos permiten manipular variables para realizar realizar acciones con ellas
+// declaraciones IF: son condiciones que le damos al codigo para que nos devuelva cierto tipo de dato o resultado segun nos convenga
+// Valores booleanos: son un tipo de valor basado en la esencia del codigo binario (0,1), indicados en js mediante true y false
